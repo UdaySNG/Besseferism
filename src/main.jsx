f7543c6654e5585ea9084d28e5dc5f97e689612e
uday buttar <Udaysngjobs@gmail.com>
2025-02-26 21:23:02 +0100

    fix deployment bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = document.createElement('div')
+root.id = 'root'
+document.body.appendChild(root)
+
+createRoot(root).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
